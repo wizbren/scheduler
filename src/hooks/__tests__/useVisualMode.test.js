fab3ff1852d5d9456689ef16cecd3513605b013b
Brendan Brown <brendan94brown@gmail.com>
2025-05-22 16:33:26 -0700

    Add THIRD test case

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -26,3 +26,21 @@ test("useVisualMode should transition to another mode", () => {
 
   expect(result.current.mode).toBe(SECOND);
 });
+
+
+const THIRD = "THIRD";
+
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => {
+    result.current.transition(SECOND);
+    result.current.transition(THIRD);
+    result.current.back();
+  })
+
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back())
+  expect(result.current.mode).toBe(FIRST)
+});
