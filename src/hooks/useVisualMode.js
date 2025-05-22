0118c524da167688f07bd43f30ad3ff00c4c3815
Brendan Brown <brendan94brown@gmail.com>
2025-05-22 16:38:51 -0700

    Format for organization

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
@@ -21,4 +22,4 @@ export default function useVisualMode(initial) {
   }
 
   return { mode, transition, back };
-}
+};
