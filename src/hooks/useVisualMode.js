c9d1e92e006f8f0461d77b5dfbd11037874bfdbd
Brendan Brown <brendan94brown@gmail.com>
2025-05-22 17:33:44 -0700

    Refactor code to use solution provided from compass assignment, getting back on track and refocusing

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,23 +4,17 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode, replace = false) {
-    setHistory(prev => 
-      replace 
-      ? [...prev.slice(0, prev.length - 1), newMode] 
-      : [...prev, newMode]
-    );
+  function transition(newMode) {
+    setHistory(prev => [...prev, newMode]);
   }
 
   function back() {
-    setHistory(prev => {
-      if (prev.length < 2) return prev;
-      return [...prev.slice(0, prev.length - 1)];
-    });
+    setHistory(prev => [...prev.slice(0, prev.length - 1)]);
   }
 
-  return { 
-    mode: history[history.length - 1], 
-    transition, 
-    back };
-};
+  return {
+    mode: history[history.length - 1],
+    transition,
+    back
+  };
+}
