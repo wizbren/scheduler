b40a915118e84c34de050a7b5c2836651a19bc9f
Brendan Brown <brendan94brown@gmail.com>
2025-05-22 20:28:07 -0700

    Add EMPTY and SHOW components to render based on mode condition

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -10,20 +10,23 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 
 export default function Appointment(props) {
-  const {mode, transition, back} = useVisualMode(props.interview ? SHOW : EMPTY);
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
 
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY && (
+        <Empty onAdd={() => transition(CREATE)} />
+      )}
+
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           onDelete={() => console.log("CONFIRM")}
           onEdit={() => console.log("EDIT")}
         />
-        :
-        <Empty onAdd={() => console.log("CREATE")} />}
+      )}
     </article>
   );
 }
