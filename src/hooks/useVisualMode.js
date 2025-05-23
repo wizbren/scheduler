dcdd895d1abeb6005509a360817c774a7eeb830e
Brendan Brown <brendan94brown@gmail.com>
2025-05-22 17:28:59 -0700

    Modify useVisualMode to remove all traces of separate mode states so that only history state is used

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,24 +2,25 @@ import { useState } from "react";
 
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   function transition(newMode, replace = false) {
-    setMode(newMode);
     setHistory(prev => 
-      replace ? [...prev.slice(0, prev.length - 1), newMode] : [...prev, newMode]
+      replace 
+      ? [...prev.slice(0, prev.length - 1), newMode] 
+      : [...prev, newMode]
     );
   }
 
   function back() {
     setHistory(prev => {
       if (prev.length < 2) return prev;
-      const newHistory = [...prev.slice(0, -1)];
-      setMode(newHistory[newHistory.length - 1])
-      return newHistory;
+      return [...prev.slice(0, prev.length - 1)];
     });
   }
 
-  return { mode, transition, back };
+  return { 
+    mode: history[history.length - 1], 
+    transition, 
+    back };
 };
