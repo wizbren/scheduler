18e799502b1ac413fadd05c30804ae71a2c5d7e7
Brendan Brown <brendan94brown@gmail.com>
2025-05-22 20:50:54 -0700

    Pass save function to Form component as onSave

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -55,6 +55,7 @@ export default function Appointment(props) {
         <Form
           interviewers={props.interviewers}
           onCancel={back}
+          onSave={save}
         />
       )}
     </article>
