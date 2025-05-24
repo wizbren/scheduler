025171ff011a40e6872184b939d3ad665d499113
Brendan Brown <brendan94brown@gmail.com>
2025-05-24 16:32:30 -0700

    Replace test with updated async await test provided with compass work

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -3,13 +3,11 @@ import { render, fireEvent } from "@testing-library/react";
 import Application from "../Application";
 
 
-describe("Application", () => {
-  it("defaults to Monday and changes the schedule when a new day is selected", () => {
-    const { findByText } = render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+  const { queryByText, getByText,findByText } = render(<Application />);
 
-    return findByText("Monday").then(() => {
-      fireEvent.click(document.querySelector("[data-testid='day'] [alt='Tuesday']"));
-      return findByText("Potato Potato");
-    });
-  });
+  await findByText("Monday");
+
+  fireEvent.click(getByText("Tuesday"));
+  expect(queryByText("Leopold Silvers")).toBeInTheDocument();
 });
