1033d263aad84d1f0b4f845a5a32a9fab8af4029
Brendan Brown <brendan94brown@gmail.com>
2025-05-23 20:54:27 -0700

    Fix interviewers array in tests to use name key instead of student

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -8,7 +8,7 @@ describe("Form", () => {
   const interviewers = [
     {
       id: 1,
-      student: "Sylvia Palmer",
+      name: "Sylvia Palmer",
       avatar: "https://i.imgur.com/LpaY82x.png"
     }
   ];
