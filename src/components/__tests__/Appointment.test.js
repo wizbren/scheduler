b68c8568e8cd47c506fabe688cf5c524fb864956
Brendan Brown <brendan94brown@gmail.com>
2025-05-24 13:04:13 -0700

    Add mock data fixtures to axios mock file

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import Appointment from "../../components/Appointment";
+import Appointment from "../Appointment";
 
 
 describe("Appointment", () => {
