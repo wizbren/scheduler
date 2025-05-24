61d6474d1225156127f466d806930e2e1b6f1748
Brendan Brown <brendan94brown@gmail.com>
2025-05-24 13:25:56 -0700

    Add mock axios get requests to test days, appointments, and interviewers

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -54,4 +54,32 @@ const fixtures = {
 };
 
 
-export default { fixtures };
+export default {
+  get: jest.fn((url) => {
+    if (url === "/api/days") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.days,
+      });
+    }
+
+    if (url === "/api/appointments") {
+      /* Resolve appointments data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments,
+      });
+    }
+
+    if (url === "/api/interviewers") {
+      /* Resolve interviewers data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers,
+      });
+    }
+  }),
+};
