e5014c01a47253d34941d9dbeb76a657a181e314
Brendan Brown <brendan94brown@gmail.com>
2025-05-25 11:55:51 -0700

    Add axios mock for testing successful deletes

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -91,4 +91,11 @@ export default {
       statusText: "No Content"
     });
   }),
+
+  delete: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    });
+  })
 };
