f1e291439697314a0c4b2444f4de6600f898166f
Brendan Brown <brendan94brown@gmail.com>
2025-05-25 11:51:26 -0700

    Add new test for checking if user can cancel interviews/increase available spots

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -47,4 +47,24 @@ describe("Application", () => {
 
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    const { container, debug } = render(<Application />);
+
+    await findByText(container, "Archie Cohen");
+
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+    expect(getByText(appointment, "Are you sure you would like to delete?")).toBeInTheDocument();
+
+    fireEvent.click(getByText(appointment, "Confirm"));
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+    await findByAltText(appointment, "Add");
+
+    const day = getAllByTestId(container, "day").find((day) => queryByText(day, "Monday"));
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+  });
 });
